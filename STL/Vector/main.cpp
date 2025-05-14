90d0eb65905c52c17fee154dcbc44d6bce4681e7
padols <spsharad.padol@gmail.com>
2025-05-14 22:21:07 +0530

    Update main.cpp

--- STL/Vector/main.cpp
+++ STL/Vector/main.cpp
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                              Online C++ Debugger.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Debug" button to debug it.
-
-*******************************************************************************/
 
 #include <iostream>
 #include<vector>
