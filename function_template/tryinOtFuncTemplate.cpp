a9e7dd87c3e9b7e09318340a08c19f1a9a99f941
padols <spsharad.padol@gmail.com>
2025-05-16 23:04:44 +0530

    Create tryinOtFuncTemplate.cpp

---
+++ function_template/tryinOtFuncTemplate.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+
+T maximum(T a, T b)
+{
+    return (a>b)?a:b;
+}
+
+int main()
+{
+    int a{5};
+    
+    int b{19};
+    
+    cout<<"int maximun :: "<< maximum(a,b)<<endl;
+    
+    double c{3.1};
+    double d{78.9};
+    
+    cout<<"double maxium :: "<< maximum(c,d)<<endl;
+    
+    string e{"hello"};
+    string f{"world"};
+    
+    cout<<"string maxium::  "<<maximum(e,f)<<endl;
+    
+    return 0;
+}
